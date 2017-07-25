db692fa5e1c79b86de431ad43b642d3ca12d6928
kite2 <dotienha@gmail.com>
2017-07-25 23:14:17 +0700

    bai chuyen tien

--- nhung-JavaScripts.js
+++ nhung-JavaScripts.js
@@ -17,7 +17,7 @@ var i = 10;
 var f = 20.5;
 var b = true;
 var s = "Ha Noi";
-function showvar() {
+function showVar() {
     document.write('i=' +i);
     document.write("<br/>");
     document.write('f=' +f)
@@ -29,10 +29,24 @@ function showvar() {
 var width = 20;
 var height = 10;
 var area = width * height;
-function showarea() {
+function showArea() {
     document.write('area=' +area);
     document.write("<br/>");
     document.write('width=' +width);
     document.write("<br/>");
     document.write('height=' +height);
 }
+function phepChia() {
+    var a = prompt("Nhập số thứ 1 vào");
+    var b = prompt("Nhập số thứ 2 vào");
+    alert("a chia b có dư là" +' '+a%b)
+}
+
+function chuyenTien() {
+    var tien1= document.getElementById('form1').elements.item(0).value;
+    var tien2= document.getElementById('form1').elements.item(1).value;
+    var tien3= document.getElementById('form1').elements.item(2).value;
+    var tien4= tien1*tien3;
+    document.getElementById('tienchuyen').innerHTML = tien4;
+}
+
